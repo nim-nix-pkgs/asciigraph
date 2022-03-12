c1cf04414d3895a2782ba1d14f041f9cf384eef7
hugosenari <hugosenari@gmail.com>
2022-03-12 00:33:22 -0300

    chore: re index asciigraph

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Console ascii line charts in pure nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-asciigraph-master.flake = false;
+  inputs.src-asciigraph-master.owner = "Yardanico";
+  inputs.src-asciigraph-master.ref   = "refs/heads/master";
+  inputs.src-asciigraph-master.repo  = "asciigraph";
+  inputs.src-asciigraph-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-asciigraph-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-asciigraph-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
