ecc40594701f25e4163aa700398b705cc85a9567
hugosenari <hugosenari@gmail.com>
2022-05-12 11:49:41 -0300

    chore: re index asciigraph

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."asciigraph-master".dir   = "master";
+  inputs."asciigraph-master".owner = "nim-nix-pkgs";
+  inputs."asciigraph-master".ref   = "master";
+  inputs."asciigraph-master".repo  = "asciigraph";
+  inputs."asciigraph-master".type  = "github";
+  inputs."asciigraph-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."asciigraph-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
